94f61c1ca920723a1622e2bcfe2ca333cbfc1af3
EshanBhaskar <eshanbhaskar1234509876@gmail.com>
2026-04-09 23:26:00 +0530

    Update PostHog tracking profile permissions to trace anonymous visitor flow

--- showcase/app/providers.tsx
+++ showcase/app/providers.tsx
@@ -5,7 +5,7 @@ import { PostHogProvider } from 'posthog-js/react'
 if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST as string,
-    person_profiles: 'identified_only', // or 'always' to track anonymous users
+    person_profiles: 'always', // Track all anonymous visitors on the portfolio
   })
 }
 
